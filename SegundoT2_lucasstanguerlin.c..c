a5cf2867f1c73ecd2eb0ca3b70acdc0ff2b06622
LUCASA87 <118824554+LUCASA87@users.noreply.github.com>
2022-12-23 10:15:51 -0300

    Update SegundoT2_lucasstanguerlin.c..c
    /* 
    * Sistemas de Informação & Análise e Desenvolvimento de Sistemas
    * Algoritmos
    
    * -------------------------
    * Objetivo do Programa:
    /* A matrícula dos alunos da Faculdade Dom Bosco é formada pelas seguintes informações: os quatro 
    (04) primeiros caracteres indicam o ano em que o aluno se matriculou na Faculdade; o próximo 
    caractere indica o semestre (1 – para o primeiro; 2 – para o segundo); o próximo caractere indica o 
    curso ( 1 – ADM; 2 – ADS; 3 – CON; 4 – ENG; 5 – DIR; 6 – SIS; 7 – PSI; 8 – LOG ) e os 
    próximos seis (06) são números aleatórios que formam identificadores únicos (esta informação não 
    é relevante para a solução do problema). Escrever um programa que leia um número indeterminado 
    de matrículas e ao final da leitura mostre as seguintes informações:
    a. total de alunos para cada curso;
    b. percentual de alunos do curso de ADS e SIS, sobre o total de alunos lidos;
    c. total de alunos matriculados por semestre;
    d. qual o curso com o menor número de matrículas
    
     
    */

--- SegundoT2_lucasstanguerlin.c..c
+++ SegundoT2_lucasstanguerlin.c..c
@@ -1,28 +1,3 @@
-/* 
-* Sistemas de Informação & Análise e Desenvolvimento de Sistemas
-* Algoritmos
-
-* -------------------------
-* Objetivo do Programa:
-/* A matrícula dos alunos da Faculdade Dom Bosco é formada pelas seguintes informações: os quatro 
-(04) primeiros caracteres indicam o ano em que o aluno se matriculou na Faculdade; o próximo 
-caractere indica o semestre (1 – para o primeiro; 2 – para o segundo); o próximo caractere indica o 
-curso ( 1 – ADM; 2 – ADS; 3 – CON; 4 – ENG; 5 – DIR; 6 – SIS; 7 – PSI; 8 – LOG ) e os 
-próximos seis (06) são números aleatórios que formam identificadores únicos (esta informação não 
-é relevante para a solução do problema). Escrever um programa que leia um número indeterminado 
-de matrículas e ao final da leitura mostre as seguintes informações:
-a. total de alunos para cada curso;
-b. percentual de alunos do curso de ADS e SIS, sobre o total de alunos lidos;
-c. total de alunos matriculados por semestre;
-d. qual o curso com o menor número de matrículas
-
- 
-*/
-
-
-
-
-
 
 #include <stdio.h>
 #include <string.h>
@@ -45,9 +20,9 @@ int main()
 	    do
 	    
 	    {
-	    	resultado=0;//zerando o res para condição da resposta.
+	    	resultado=0;//zerando o res para condiÃ§Ã£o da resposta.
 	    	cont++;
-	        printf("Digite a matrícula de 12 números, após digitar cada número pressione ENTER.\n");
+	        printf("Digite a matrÃ­cula de 12 nÃºmeros, apÃ³s digitar cada nÃºmero pressione ENTER.\n");
 	        for(i=0;i<x;i++)
 	        {
 	            scanf("%d",&mat[i]);//entrada de dados.
@@ -139,7 +114,7 @@ int main()
 			while(resultado!=1)
 			{
 				setbuf(stdin, NULL);
-	    		printf("Resposta inválida digite S/N para continuar.\n " );
+	    		printf("Resposta invÃ¡lida digite S/N para continuar.\n " );
 				scanf("%c",&cond);	
 				if ((cond=='s')||(cond=='S'))
 		   	{
@@ -167,43 +142,43 @@ int main()
 		 printf("\n-------------------------------------------------------------------------------------------------\n\n");
 		
 		//b. percentual de alunos do curso de ADS e SIS, sobre o total de alunos lidos;
-		printf("O percentual de alunos do curso de ADS é %.2f e em SIS é %.2f.\n", PADS,PSIS);
+		printf("O percentual de alunos do curso de ADS Ã© %.2f e em SIS Ã© %.2f.\n", PADS,PSIS);
 		printf("\n-------------------------------------------------------------------------------------------------\n\n");
 		//c. Total de alunos matriculados por semestre;
-		printf("O total de aluno matriculado por semestre é:\n1° Semestre: %d e 2° Semestre: %d.\n",sm1,sm2);
+		printf("O total de aluno matriculado por semestre Ã©:\n1Â° Semestre: %d e 2Â° Semestre: %d.\n",sm1,sm2);
 		printf("\n-------------------------------------------------------------------------------------------------\n\n");
-		//d. qualprintf("\n\n"); o curso com o menor número de matrículas;
+		//d. qualprintf("\n\n"); o curso com o menor nÃºmero de matrÃ­culas;
 		if(c1<c2 && c1<c3 && c1<c4 && c1<c5 && c1<c6 && c1<c7&& c1<c8)//Teste curso 1
 		{
-			printf("O curso com o maior números de matriculas é o ADM.");
+			printf("O curso com o maior nÃºmeros de matriculas Ã© o ADM.");
 		}
 		else  if(c2<c1 && c2<c3 && c2<c4 && c2<c5 && c2<c6 && c2<c7 && c2<c8)//teste curso 2
 		{
-			printf("O curso com o maior números de matriculas é o ADS.");
+			printf("O curso com o maior nÃºmeros de matriculas Ã© o ADS.");
 		}
 		else  if(c3<c1 && c3<c2 && c3<c4 && c3<c5 && c3<c6 && c3<c7 && c3<c8)//teste curso 3
 		{
-			printf("O curso com o maior números de matriculas é o CON.");
+			printf("O curso com o maior nÃºmeros de matriculas Ã© o CON.");
 		}
 		else if(c5<c1 && c5<c2 && c5<c3 && c5<c4 && c5<c6&& c5<c7 && c5<c8)//teste curso 5
 		{
-			printf("O curso com o maior números de matriculas é o DIR.");
+			printf("O curso com o maior nÃºmeros de matriculas Ã© o DIR.");
 		}
 		else if(c6<c1 && c6<c2 && c6<c3 && c6<c4 && c6<c5 && c6<c7 && c6<c8)//teste curso 6
 		{
-			printf("O curso com o maior números de matriculas é o SIS.");
+			printf("O curso com o maior nÃºmeros de matriculas Ã© o SIS.");
 		}
 		else if(c7<c1 && c7<c2 && c7<c3 && c7<c4 && c7<c5 && c7<c6 && c7<c8)//teste curso 7
 		{
-			printf("O curso com o maior números de matriculas é o PSI.");
+			printf("O curso com o maior nÃºmeros de matriculas Ã© o PSI.");
 		}
 		else if(c8<c1 && c8<c2 && c8<c3 && c8<c4&& c8<c5&& c8<c6&& c8<c7)//teste curso 8
 		{
-			printf("O curso com o maior números de matriculas é o LOG.");
+			printf("O curso com o maior nÃºmeros de matriculas Ã© o LOG.");
 		}
 		else 
 		{
-			printf("Houve mais de um curso com o número iguais de matrículas.");
+			printf("Houve mais de um curso com o nÃºmero iguais de matrÃ­culas.");
 		}
 		
 	
